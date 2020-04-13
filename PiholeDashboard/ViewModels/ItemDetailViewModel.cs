57b922deda3e216f2bf6a0e30160600653ca506d
joshspicer <hello@joshspicer.com>
2020-04-13 08:49:07 -0400

    init

---
+++ PiholeDashboard/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PiholeDashboard.Models;
+
+namespace PiholeDashboard.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
