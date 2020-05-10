73d93cbda612b7e0dce8f28de268230ec635470d
joshspicer <hello@joshspicer.com>
2020-05-10 11:57:45 -0400

    appcenter config

--- PiholeDashboard/App.xaml.cs
+++ PiholeDashboard/App.xaml.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-using PiholeDashboard.Views;
+﻿using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 
 namespace PiholeDashboard
 {
@@ -17,6 +18,15 @@ namespace PiholeDashboard
 
         protected override void OnStart()
         {
+
+            string appcenterID = Environment.GetEnvironmentVariable("appcenterID");
+
+            if (appcenterID != null && appcenterID.Length > 5)
+            {
+                AppCenter.Start($"ios={appcenterID}",
+                    typeof(Analytics), typeof(Crashes));
+            }
+
         }
 
         protected override void OnSleep()
